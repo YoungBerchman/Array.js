3ddee755f0f942db74b3f97e1fcc83ab6a23ff67
YoungBerchman <praiseegbe99@gmail.com>
2024-11-09 23:10:55 +0100

    javascript file

---
+++ array.js
@@ -0,0 +1,28 @@
+const students = [
+    {
+        name: "Alice",
+        age: 22,
+        scores: {math:85,english:92,science:88},
+        courses:["Math","English","Science"],
+    },
+    {
+        name:"Bob",
+        age:24,
+        scores:{math:78,english:85,science:90},
+        courses:["Math","History","Science"],
+    },
+    {
+        name:"Charlie",
+        age: 23,
+        scores:{Math:92,english:88,science:95},
+        courses:["Math","English","Art"],
+    },
+    {
+        name:"David",
+        age:25,
+        scores:{math:95,english:82,science:80},
+        courses:["Math","English","Science","Art"],
+    },
+];
+console.log(students);
+console.log(students.forEach((items)=>l));
